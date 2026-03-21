be5b16dd8c4ad679fd128d6e3eee78a7f68c263d
OneTrueHomie <vicigwilo@gmail.com>
2026-03-21 23:54:44 +0100

    feat(video): register AegisMarketing and BatchDemoResults in Root

--- video/src/Root.tsx
+++ video/src/Root.tsx
@@ -1,5 +1,7 @@
 import { Composition } from 'remotion';
 import { AegisDemo } from './AegisDemo';
+import { AegisMarketing } from './AegisMarketing';
+import { MARKETING_FPS, MARKETING_TOTAL_FRAMES } from './marketing/slides';
 
 export const RemotionRoot: React.FC = () => {
   return (
@@ -19,6 +21,23 @@ export const RemotionRoot: React.FC = () => {
         component={AegisDemo}
         durationInFrames={150 * 30}
         fps={30}
+        width={1080}
+        height={1080}
+      />
+      {/* 1-minute hackathon pitch: 15 slides × 4s */}
+      <Composition
+        id="AegisMarketing"
+        component={AegisMarketing}
+        durationInFrames={MARKETING_TOTAL_FRAMES}
+        fps={MARKETING_FPS}
+        width={1920}
+        height={1080}
+      />
+      <Composition
+        id="AegisMarketing-Square"
+        component={AegisMarketing}
+        durationInFrames={MARKETING_TOTAL_FRAMES}
+        fps={MARKETING_FPS}
         width={1080}
         height={1080}
       />
